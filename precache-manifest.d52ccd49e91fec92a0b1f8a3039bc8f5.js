447948111025bf95cbcae9a7820a96a4119e01fc
Neely Anne <neely.anne@yahoo.com>
2020-01-07 19:24:44 -0500

    Updates

---
+++ precache-manifest.d52ccd49e91fec92a0b1f8a3039bc8f5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78711762e2559e814c723428935987b",
+    "url": "/NeelyAnne/clicky.git/index.html"
+  },
+  {
+    "revision": "39c21923ab30e0c36c7b",
+    "url": "/NeelyAnne/clicky.git/static/css/main.d67044ec.chunk.css"
+  },
+  {
+    "revision": "bf301ef91b34fb51d9f2",
+    "url": "/NeelyAnne/clicky.git/static/js/2.bd3a4475.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/NeelyAnne/clicky.git/static/js/2.bd3a4475.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39c21923ab30e0c36c7b",
+    "url": "/NeelyAnne/clicky.git/static/js/main.9534596c.chunk.js"
+  },
+  {
+    "revision": "295ac5342adad04b5ea3",
+    "url": "/NeelyAnne/clicky.git/static/js/runtime-main.79d882b4.js"
+  },
+  {
+    "revision": "ec033b99d1059e902ae1af31d544ce86",
+    "url": "/NeelyAnne/clicky.git/static/media/FamousApps.ec033b99.jpg"
+  },
+  {
+    "revision": "0d7c41c6a3ad7ab4d6b0d27f0d2d5704",
+    "url": "/NeelyAnne/clicky.git/static/media/airbnb.0d7c41c6.jpg"
+  },
+  {
+    "revision": "778756251dcf59b565e589bec41e01c0",
+    "url": "/NeelyAnne/clicky.git/static/media/hulu.77875625.jpg"
+  },
+  {
+    "revision": "bcdcb63d089e822117b59ae32f775f03",
+    "url": "/NeelyAnne/clicky.git/static/media/insta.bcdcb63d.jpg"
+  },
+  {
+    "revision": "b8c5b5bdb7e81f23d60f1548681d6b48",
+    "url": "/NeelyAnne/clicky.git/static/media/pinterest2.b8c5b5bd.jpeg"
+  },
+  {
+    "revision": "368d1c34c39a228088a4c8411232d0cc",
+    "url": "/NeelyAnne/clicky.git/static/media/snap.368d1c34.jpg"
+  },
+  {
+    "revision": "349e98df3f57e636bbb3625ab70e7bc0",
+    "url": "/NeelyAnne/clicky.git/static/media/twitter.349e98df.png"
+  },
+  {
+    "revision": "a47c912bda8d24a513016791f955f31a",
+    "url": "/NeelyAnne/clicky.git/static/media/venmo.a47c912b.png"
+  }
+]);
